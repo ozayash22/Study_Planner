824eaef5a5096e305c879bce1e87523095565977
ozayash22 <ozayash222@gmail.com>
2025-08-22 20:12:09 +0530

    added the frontend build folder

---
+++ study_planner/src/main/java/com/studyPlanner/study_planner/controller/FrontendController.java
@@ -0,0 +1,14 @@
+package com.studyPlanner.study_planner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String redirect() {
+        // Forwards any unmapped paths (except those containing a period) to index.html
+        return "forward:/index.html";
+    }
+}
--- study_planner_frontend/src/api/axiosInstance.js
+++ study_planner_frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api", // ✅ your Spring Boot base API URL
+  baseURL: "api", // ✅ your Spring Boot base API URL
 });
 
 // ✅ Automatically attach token to every request
